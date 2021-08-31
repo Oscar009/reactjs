a5e2205e97edc284d580f53a12b3e95087faea44
Oscar Noe Ortiz Barba <oortizbarba@gmail.com>
2021-08-31 15:51:11 -0500

    Creacion de componentes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+function HelloWorld(){
+  return(
+    <div id="hello">Hello World!!</div>
+  );
+}
+
+//const App = () => <div>This is my component: <HelloWorld/></div>
+
+/* class App extends React.Component {
+  render(){
+    return <div>This is my component: <HelloWorld/></div>
+  }
+}
+ */
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div>This is my component: <HelloWorld/> <HelloWorld/></div>;
 }
 
 export default App;
