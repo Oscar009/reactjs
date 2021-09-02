45fcdf4b3948fdc5832aa910d82bd753d0e2890e
Oscar Noe Ortiz Barba <oortizbarba@gmail.com>
2021-09-01 19:35:00 -0500

    Styles en Reactjs

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,16 +1,36 @@
 import React, { Component } from "react";
+import "./Tasks.css";
 
 class Task extends Component {
+
+  StyleCompleted(){
+    return{
+      fonrSize: '20px',
+      color: this.props.task.done ? 'gray' : 'black',
+      textDecoration: this.props.task.done ? 'line-throug' : 'none' 
+    }
+  }
+
   render() {
     const { task } = this.props;
     return (
-      <div>
+      <div style={this.StyleCompleted()}>
         {task.title} - {task.description} - {task.done} - {task.id}
         <input type="checkbox" />
-        <button>x</button>
+        <button style={btnDelete}>x</button>
       </div>
     );
   }
 }
+
+const btnDelete = {
+  fontSize: '18px',
+  background: '#ea2027',
+  color: '#fff',
+  border: 'none',
+  padding: '10px 15px',
+  borderRadius: '50%',
+  cursor: 'pointer'
+};
 
 export default Task;
